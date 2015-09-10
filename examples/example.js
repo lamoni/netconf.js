729b2211977aa46ba1526df1fe72a9bf148875fc
Lamoni Finlayson <lamonif@gmail.com>
2015-09-10 13:30:25 -0400

    Updated

--- examples/example.js
+++ examples/example.js
@@ -1,5 +1,17 @@
 var Netconf = require('../lib/netconf');
 
-var nc = new Netconf('localhost', {});
+var nc = new Netconf({
+    host: '172.16.200.129',
+    username: 'root',
+    password: 'root123'
+});
 
-console.log(nc.connect());
+nc.connect(
+    function () {
+        nc.sendOperationalCommandXML('show interfaces terse');
+    },
+
+    function(output) {
+        console.log(output);
+    });
+
