413ad835481f5b7e4c04577f2e8da41276eb375e
Lamoni Finlayson <lamonif@gmail.com>
2015-09-10 22:02:21 -0400

    Revamped the entire structure / behavior of code.  Still need massive clean up but it's at a good break point

--- examples/example.js
+++ examples/example.js
@@ -1,17 +1,26 @@
 var Netconf = require('../lib/netconf');
 
 var nc = new Netconf({
-    host: '172.16.200.129',
-    username: 'root',
-    password: 'root123'
+    hosts: {
+        '172.16.200.129': {
+            username: 'lamoni',
+            password: 'lamoni123',
+            mode: 'multiple'
+        },
+        '172.16.200.131': {
+            username: 'lamoni',
+            password: 'lamoni123',
+            mode: 'single'
+        }
+    }
 });
 
-nc.connect(
-    function () {
-        nc.sendOperationalCommandXML('show interfaces terse');
-    },
+nc.sendOperationalCommand('text', 'show version', function (hostname, output) {
 
-    function(output) {
+    console.log(output);
+    nc.sendOperationalCommandToSpecificHost(hostname, 'text', 'show system uptime', function (hostname, output) {
         console.log(output);
     });
+
+});
 
